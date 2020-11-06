ff33e9587fbbcce407d8ea4e96271881a1e33a17
kt87m <t.past.future@gmail.com>
2020-11-06 15:46:45 +0900

    タスク更新のe2eテストを追加、既存テスト修正

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -13,14 +13,49 @@ context('top page', () => {
   });
 
   it('can transition to task detail', () => {
-    cy.get('.linkToDetail[data-task-id="1"]').click();
+    cy.get('[data-task-id="1"] .linkToDetail').click();
     cy.url().should('include', '/tasks/1');
   });
 });
 
 context('task detail page', () => {
   it('display specified task', () => {
-    cy.visit('/tasks/1').contains('.taskTitle', 'テストタスク');
-    cy.visit('/tasks/2').contains('.taskTitle', 'テストタスク2');
+    cy.visit('/tasks/1')
+      .get('.taskTitle input')
+      .should('has.value', 'テストタスク');
+    cy.visit('/tasks/2')
+      .get('.taskTitle input')
+      .should('has.value', 'テストタスク2');
+  });
+});
+
+context('update task state globaly', () => {
+  it('toggle task execution state', () => {
+    cy.visit('/');
+    cy.get('[data-task-id="1"] input[type="checkbox"]')
+      .as('checkbox')
+      .check()
+      .should('have.attr', 'checked');
+
+    cy.visit('/tasks/1');
+    cy.get('input[type="checkbox"]')
+      .as('checkbox_detail')
+      .should('have.attr', 'checked');
+    cy.get('@checkbox_detail').uncheck();
+
+    cy.visit('/');
+    cy.get('@checkbox').should('not.have.attr', 'checked');
+  });
+
+  it('change task title', () => {
+    cy.visit('/tasks/1');
+    cy.get('.taskTitle input')
+      .type('タイトル変更')
+      .screenshot('task_title_typed');
+
+    cy.visit('/');
+    cy.get('[data-task-id="1"]')
+      .contains('タイトル変更')
+      .screenshot('task_title_changed_in_top_page');
   });
 });
