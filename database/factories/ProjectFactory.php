5565224be0de40b11a8f5b77059156b4414236c3
kt87m <t.past.future@gmail.com>
2020-11-20 20:40:25 +0900

    Projectファクトリ追加

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProjectFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Project::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence($nbWords = 3, $variableNbWords = true),
+            'expiration' => date( 'Y-m-d H:i:s', strtotime('+1 hour') ),
+        ];
+    }
+}
