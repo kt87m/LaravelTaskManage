51af18b18b3fb42073176e9ecdb835112f6ee5c2
kt87m <t.past.future@gmail.com>
2020-12-03 21:46:52 +0900

    改行、フォーマット

--- resources/ts/components/InfoArea.tsx
+++ resources/ts/components/InfoArea.tsx
@@ -8,22 +8,22 @@ const InfoArea: React.FC = () => {
   if (!/project_id=/.test(location.search))
     content = <p className="p-3 pl-0">タスクを追加してプロジェクトを開始</p>;
   else
-    content = <>
-      <p className="p-3 pl-0">未保存のプロジェクト</p>
-      <Link
-        to="/"
-        className="block ml-auto p-3 text-blue-500 hover:bg-gray-300 transition-all"
-      >
-        + 新規プロジェクト
-      </Link>
-    </>;
+    content = (
+      <>
+        <p className="p-3 pl-0">未保存のプロジェクト</p>
+        <Link
+          to="/"
+          className="block ml-auto p-3 text-blue-500 hover:bg-gray-300 transition-all"
+        >
+          + 新規プロジェクト
+        </Link>
+      </>
+    );
 
   return (
     <div className="InfoArea bg-gray-200 h-12">
       <div className="container mx-auto">
-        <div className="flex text-gray-600">
-          {content}
-        </div>
+        <div className="flex text-gray-600">{content}</div>
       </div>
     </div>
   );
