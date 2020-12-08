99cff9aaa50f3eee337e253250dd20cfd4277a08
kt87m <t.past.future@gmail.com>
2020-12-08 22:14:43 +0900

    プロジェクト保存のテスト追加

---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Preserve project permanently
+     *
+     * @return void
+     */
+    public function testPreserveProject()
+    {
+        $project = Project::factory()->create();
+        $this->assertFalse($project->preserved);
+        
+        $project->preserve();
+
+        $this->assertTrue($project->preserved);
+        $this->assertEquals(Project::EXPIRATION_MAX, $project->expiration);
+    }
+}
