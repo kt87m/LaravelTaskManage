cc420564bb2d9d8da7ce5f66765097f9ad4f54e1
kt87m <t.past.future@gmail.com>
2021-01-04 19:47:44 +0900

    test(e2e): apply change of uri format

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import { task1, task2, queryString, setupDb } from '../fixtures/serverResource';
+import { task1, task2, project, setupDb } from '../fixtures/serverResource';
 
 context('top page', () => {
   beforeEach(() => {
@@ -8,7 +8,7 @@ context('top page', () => {
   });
 
   it('list sample task', () => {
-    cy.visit(`/${queryString}`);
+    cy.visit(`/projects/${project.id}`);
     cy.get('li').should(($li) => {
       expect($li).to.have.length(2);
       expect($li).contain(task1.title);
@@ -16,9 +16,9 @@ context('top page', () => {
   });
 
   it('can transition to task detail', () => {
-    cy.visit(`/${queryString}`);
+    cy.visit(`/projects/${project.id}`);
     cy.get(`[data-task-id="${task1.id}"] .linkToDetail`).click();
-    cy.url().should('include', `/tasks/${task1.id}`);
+    cy.url().should('include', `/projects/${project.id}/tasks/${task1.id}`);
   });
 });
 
@@ -31,16 +31,16 @@ context('task detail page', () => {
     cy.visit(`/tasks/${task1.id}`)
       .wait(1000)
       .contains('URLにプロジェクトIDが含まれていません');
-    cy.visit(`/tasks/${task1.id}${queryString}`)
+    cy.visit(`/projects/${project.id}/tasks/${task1.id}`)
       .get('.taskTitle input')
       .should('has.value', task1.title);
   });
 
   it('display specified task', () => {
-    cy.visit(`/tasks/${task1.id}${queryString}`)
+    cy.visit(`/projects/${project.id}/tasks/${task1.id}`)
       .get('.taskTitle input')
       .should('has.value', task1.title);
-    cy.visit(`/tasks/${task2.id}${queryString}`)
+    cy.visit(`/projects/${project.id}/tasks/${task2.id}`)
       .get('.taskTitle input')
       .should('has.value', task2.title);
   });
@@ -52,28 +52,28 @@ context('update task state globaly', () => {
   });
 
   it('toggle task execution state', () => {
-    cy.visit(`/${queryString}`);
+    cy.visit(`/projects/${project.id}`);
     cy.get(`[data-task-id="${task1.id}"] input[type="checkbox"]`)
       .as('checkbox')
       .check()
       .should('be.checked');
 
-    cy.visit(`/tasks/${task1.id}${queryString}`);
+    cy.visit(`/projects/${project.id}/tasks/${task1.id}`);
     cy.wait(500) // wait task loading
       .get('input[type="checkbox"]')
       .as('checkbox_detail')
       .should('be.checked');
     cy.get('@checkbox_detail').uncheck().should('not.be.checked');
 
-    cy.visit(`/${queryString}`);
+    cy.visit(`/projects/${project.id}`);
     cy.get('@checkbox').should('not.be.checked');
   });
 
   it('change task title', () => {
-    cy.visit(`/tasks/${task1.id}${queryString}`);
+    cy.visit(`/projects/${project.id}/tasks/${task1.id}`);
     cy.get('.taskTitle input').clear().type('タイトル変更').wait(1000); // wait task updating
 
-    cy.visit(`/${queryString}`);
+    cy.visit(`/projects/${project.id}`);
     cy.get(`[data-task-id="${task1.id}"]`).contains('タイトル変更');
   });
 });
@@ -84,7 +84,7 @@ context('create new task', () => {
   });
 
   it('create new task with title placeholder "名称未設定タスク"', () => {
-    cy.visit(`/${queryString}`);
+    cy.visit(`/projects/${project.id}`);
     cy.get('.createTask').click();
     cy.wait(1000) // wait task creating
       .get('li')
@@ -101,12 +101,11 @@ context('delete task', () => {
   });
 
   it('delete specified task', () => {
-    cy.visit(`/tasks/${task1.id}${queryString}`);
+    cy.visit(`/projects/${project.id}/tasks/${task1.id}`);
     cy.get('.deleteTask').click();
     cy.wait(1000) // wait task deleting
       .location('pathname')
-      .should('eq', `/`);
-    cy.location('search').should('eq', `${queryString}`);
+      .should('eq', `/projects/${project.id}`);
     cy.get('li').should(($li) => {
       expect($li).to.have.length(1);
       expect($li).contain(task2.title);
