f748203c14bf9b54ad8408f4fcfee545b6702d24
kt87m <t.past.future@gmail.com>
2021-01-10 23:32:45 +0900

    fix(api): modify to use route parameter to pass resource id

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('tasks', TaskController::class)->middleware('project');
-Route::apiResource('projects', ProjectController::class)->parameters([
+Route::apiResource('projects/{project_id}/tasks', TaskController::class, )->middleware('project');
+Route::get('tasks', [TaskController::class, 'index'])->middleware('project')->name('no_project_tasks.index');
+Route::post('tasks', [TaskController::class, 'store'])->middleware('project')->name('no_project_tasks.store');
+
+Route::apiResource('projects', ProjectController::class, [
+    'only' => ['show', 'update'],
+])->parameters([
     'projects' => 'project_id',
 ])->middleware('project');
