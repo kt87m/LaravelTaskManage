0ef26e0b0a8e40010a954a70534d74c96317672d
kt87m <t.past.future@gmail.com>
2021-01-12 17:48:42 +0900

    test(frontend): key/url generation

--- resources/ts/tests/useResource.spec.tsx
+++ resources/ts/tests/useResource.spec.tsx
@@ -20,14 +20,44 @@ jest.mock('axios');
 import axios from 'axios';
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-describe('tasks', () => {
+describe('generate swr key and request url', () => {
   afterEach(() => {
     jest.clearAllMocks();
     cache.clear();
   });
 
-  it('call api with passed id', () => {
-    renderHook(() => useResource('tasks').get(24));
-    expect(mockedAxios.get.mock.calls[0][0]).toMatch(/24$/);
+  test('if pathname does not include resource type, use the pathname with it appended', () => {
+    history.pushState(null, '', '/xxx/yyy');
+    renderHook(() => useResource('tasks').index());
+    expect(mockedUseSWR.mock.calls[0][0]).toBe('/xxx/yyy/tasks');
+    expect(mockedAxios.get.mock.calls[0][0]).toBe('/api/xxx/yyy/tasks');
+  });
+
+  test('if pathname includes resource type, use substring of the pathname up to resource type', () => {
+    history.pushState(null, '', '/xxx/yyy/tasks/zzz');
+    renderHook(() => useResource('tasks').index());
+    expect(mockedUseSWR.mock.calls[0][0]).toBe('/xxx/yyy/tasks');
+    expect(mockedAxios.get.mock.calls[0][0]).toBe('/api/xxx/yyy/tasks');
+  });
+
+  test('if pathname does not include resource type, use `/type/id` format', () => {
+    history.pushState(null, '', '/xxx/yyy');
+    renderHook(() => useResource('tasks').get(72));
+    expect(mockedUseSWR.mock.calls[0][0]).toBe('/tasks/72');
+    expect(mockedAxios.get.mock.calls[0][0]).toBe('/api/tasks/72');
+  });
+
+  test('if pathname includes `/type/id` fragment, use substring of the pathname up to `/type/id` fragment', () => {
+    history.pushState(null, '', '/xxx/yyy/tasks/72/zzz');
+    renderHook(() => useResource('tasks').get(72));
+    expect(mockedUseSWR.mock.calls[0][0]).toBe('/xxx/yyy/tasks/72');
+    expect(mockedAxios.get.mock.calls[0][0]).toBe('/api/xxx/yyy/tasks/72');
+  });
+
+  test('generated key does not include query string', () => {
+    history.pushState(null, '', '/xxx/yyy/tasks?done=1');
+    renderHook(() => useResource('tasks').index());
+    expect(mockedUseSWR.mock.calls[0][0]).toBe('/xxx/yyy/tasks');
+    expect(mockedAxios.get.mock.calls[0][0]).toBe('/api/xxx/yyy/tasks?done=1');
   });
 });
