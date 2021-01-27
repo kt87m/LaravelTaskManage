3cfc68ab36e6fd04f3acd8706944263780c415a9
kt87m <t.past.future@gmail.com>
2021-01-27 23:37:19 +0900

    test(api): add test for task creation

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Unit;
 
-use App\Models\Project;
 use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -20,7 +19,22 @@ class TaskTest extends TestCase
         $this->task = Task::create([
             'title' => 'test',
             'done' => false,
+            'description' => 'test description',
+            'priority' => 2,
+            'duedate' => '2020/1/1 00:00:00',
         ]);
+    }
+
+    /**
+     * 生成結果の確認
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $this->assertEquals('test description', $this->task->description);
+        $this->assertEquals(2, $this->task->priority);
+        $this->assertEquals('2020/1/1 00:00:00', $this->task->duedate);
     }
 
     public function testGetTaskNotExists()
