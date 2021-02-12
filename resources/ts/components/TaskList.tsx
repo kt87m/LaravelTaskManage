cb53e1fa445dff8c2f73641752b207a228358cbd
kt87m <t.past.future@gmail.com>
2021-02-12 20:54:15 +0900

    chore: improve mobile layout

--- resources/ts/components/TaskList.tsx
+++ resources/ts/components/TaskList.tsx
@@ -45,12 +45,13 @@ const TaskList: React.FC = () => {
           <li
             key={task.id}
             data-task-id={task.id}
-            className="flex pl-3 border-b-2 border-gray-200 duration-100 hover:bg-blue-100"
+            className="flex items-start pl-3 border-b-2 border-gray-200 duration-100 hover:bg-blue-100"
           >
             <Checkbox
               checked={task.done}
               onChange={(e) => onToggleCheck(e, task.id)}
               className="checked:bg-blue-500 focus:border-blue-700 focus-checked:bg-blue-700"
+              wrapperClassName="mt-4 md:mt-0 md:self-center"
             />
             <Link
               to={{
@@ -58,21 +59,25 @@ const TaskList: React.FC = () => {
                 search: location.search,
                 state: { fromTop: true },
               }}
-              className={`linkToDetail group flex items-center flex-grow ml-3 p-3 pl-0 outline-none ${
+              className={`linkToDetail overflow-hidden group flex items-center flex-wrap md:flex-no-wrap flex-grow ml-3 p-3 pl-0 outline-none ${
                 task.title ? '' : 'text-gray-500'
               } focus:text-blue-500`}
             >
-              {task.priority > 1 && (
-                <FaFlag
-                  className={`mr-2 text-${
-                    ['blue', 'yellow', 'red'][task.priority - 2]
-                  }-500`}
-                />
-              )}
-              {task.title || '名称未設定タスク'}
+              <div className="overflow-hidden flex items-center">
+                {task.priority > 1 && (
+                  <FaFlag
+                    className={`flex-shrink-0 mr-2 text-${
+                      ['blue', 'yellow', 'red'][task.priority - 2]
+                    }-500`}
+                  />
+                )}
+                <div className="truncate">
+                  {task.title || '名称未設定タスク'}
+                </div>
+              </div>
               {task.duedate && (
-                <div className="flex items-center ml-auto text-sm">
-                  <CgSandClock className="mr-1 text-xl text-gray-500" />
+                <div className="w-full md:w-auto ml-auto mt-2 md:mt-0 flex items-center text-xs md:text-sm">
+                  <CgSandClock className="-ml-1 mr-1 text-base md:text-xl text-gray-500" />
                   {format(task.duedate, 'yyyy/MM/dd HH:mm')}
                 </div>
               )}
