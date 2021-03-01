e7a8227cae7ceaa775d4ad2143ef92142ee964f2
kt87m <t.past.future@gmail.com>
2021-03-01 18:58:16 +0900

    chore: include analytics code from env

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    {!! config('app.ga') !!}
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
